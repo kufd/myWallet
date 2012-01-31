cf3f0ff749c03c7115ebbdce85dc746185fdee2e
kufd <km@ub.ua>
2012-01-31 23:49:24 +0200

    remove logs

--- application/logs/2011/12/19.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2011-12-19 11:19:13 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
-2011-12-19 11:19:13 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
---
-#0 /home/kufd/www/myWallet/index.php(108): Kohana_Request->execute()
-#1 {main}
